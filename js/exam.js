02d14265d7e40ae6ba1de31f84e7e2370d5a40f9
Will Lowry <william.c.lowry@gmail.com>
2014-08-21 15:42:28 -0400

    add basic html and styling begin work on modal

--- js/exam.js
+++ js/exam.js
@@ -1,1 +1,46 @@
 // Place your JavaScript for the exam in this file.
+
+
+
+$(document).ready(function(){
+		var
+    $overlay,
+    $modal,
+    $content,
+    $open,
+    $close;
+
+		$overlay = $('<div id="overlay"></div>');
+		$modal = $('<div id="modal"></div>');
+		$content = $('<div id="content"></div>');
+		$close = $('<a id="close" href="#">close</a>');
+
+		$modal.hide();
+		$overlay.hide();
+		$modal.append($content, $close);
+
+		
+		// $('body').append($overlay, $modal);
+	
+
+
+    $("container").on("click", $open, function() {
+	    $content.empty().append(settings.content);
+
+	    // $modal.css({
+	    //     width: settings.width || 'auto', 
+	    //     height: settings.height || 'auto'
+	    // })
+
+	    $modal.show();
+	    $overlay.show();
+		});
+
+    // Close the modal
+    $("container").on("click", $close, function () {
+	    $modal.hide();
+	    $overlay.hide();
+	    $content.empty();
+		});
+
+});
